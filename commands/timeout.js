7cb23cf33c267dd60ec321aad8faf13e5bbca0a8
dotxyz <111662337+microwavexyz@users.noreply.github.com>
2024-06-17 18:14:03 -0600

    Fixed bug

--- commands/timeout.js
+++ commands/timeout.js
@@ -120,8 +120,10 @@ module.exports = {
         .setColor(0x00FF00)
         .setTitle('User Timed Out')
         .setDescription(`Timed out ${user.tag} for ${durationInput} for: ${reason}`)
-        .addField('Case Number', `${caseNumber}`, true)
-        .addField('Moderator', `${moderator}`, true)
+        .addFields(
+          { name: 'Case Number', value: `${caseNumber}`, inline: true },
+          { name: 'Moderator', value: `${moderator}`, inline: true }
+        )
         .setTimestamp();
 
       await interaction.reply({ embeds: [embed], ephemeral: false });
